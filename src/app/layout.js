2334e6ada574e9d18634b830e5d689555eceab97
Shagor <infinityshagor18557@gmail.com>
2026-02-17 23:31:57 +0600

    connecting database, components etc

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,14 +1,10 @@
-import { Geist, Geist_Mono } from "next/font/google";
+import { Geist, Geist_Mono, Poppins } from "next/font/google";
 import "./globals.css";
+import Navbar from "@/components/layouts/Navbar";
+import Footer from "@/components/layouts/Footer";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+const poppins = Poppins({
+  weight: ["100", "200", "400", "500", "600", "800"],
 });
 
 export const metadata = {
@@ -19,10 +15,14 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
+      <body className={`${poppins.variable} antialiased`}>
+        <header className="py-2 md:w-11/12 mx-auto">
+          <Navbar />
+        </header>
+        <main className="py-2 md:w-11/12 mx-auto">{children} </main>
+        <footer className="py-2 md:w-11/12 mx-auto">
+          <Footer />
+        </footer>
       </body>
     </html>
   );
